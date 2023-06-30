a182bd3d6a0aa2a9a41063713a919838efee01bb
Pablo Deymonnaz <deymonnaz@gmail.com>
2023-06-30 12:15:06 -0300

    upgrading lambdaworks_cairo_prover version

--- watcher_dispatcher/native/watcher_dispatcher/src/cairo.rs
+++ watcher_dispatcher/native/watcher_dispatcher/src/cairo.rs
@@ -6,15 +6,15 @@ use rustler::Binary;
 const GRINDING_FACTOR: u8 = 10;
 
 #[rustler::nif]
-// Loads the program in path, runs it with the Cairo VM, and makes a proof of it
-pub fn run_program_and_get_proof_from_path(file_path: Binary) -> Vec<u8> {
-    let file_path = std::str::from_utf8(file_path.as_slice()).unwrap();
-    run_program_and_get_proof_internal(file_path)
+/// Loads the program in path, runs it with the Cairo VM, and makes a proof of it
+pub fn run_program_and_get_proof_from_path(program_content_binary: Binary) -> Vec<u8> {
+    let program_content = &program_content_binary as &[u8];
+    run_program_and_get_proof_internal(program_content)
 }
 
-pub fn run_program_and_get_proof_internal(file_path: &str) -> Vec<u8> {
+pub fn run_program_and_get_proof_internal(program_content: &[u8]) -> Vec<u8> {
     let (main_trace, cairo_air, mut pub_inputs) =
-        generate_prover_args(file_path, &CairoVersion::V1, &None, GRINDING_FACTOR).unwrap();
+        generate_prover_args(program_content, &CairoVersion::V1, &None, GRINDING_FACTOR).unwrap();
 
     let proof = prove(&main_trace, &cairo_air, &mut pub_inputs).unwrap();
     let ret: Vec<u8> = proof.serialize();
@@ -25,7 +25,8 @@ pub fn run_program_and_get_proof_internal(file_path: &str) -> Vec<u8> {
 mod test {
     #[test]
     fn test_run_program_and_get_proof() {
-        let ret = super::run_program_and_get_proof_internal("../../programs/fibonacci_cairo1.casm");
+        let program_content = std::fs::read("../../programs/fibonacci_cairo1.casm").unwrap();
+        let ret = super::run_program_and_get_proof_internal(&program_content);
         println!("ret len: {}", ret.len());
     }
 }
