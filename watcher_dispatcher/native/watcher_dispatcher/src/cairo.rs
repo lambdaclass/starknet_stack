91c75abdcaccf4b304d6a6966cff86c8662f4c4a
Pablo Deymonnaz <deymonnaz@gmail.com>
2023-07-03 17:21:05 -0300

    firsts steps of verify function

--- watcher_dispatcher/native/watcher_dispatcher/src/cairo.rs
+++ watcher_dispatcher/native/watcher_dispatcher/src/cairo.rs
@@ -1,6 +1,8 @@
-use lambdaworks_math::traits::Serializable;
+use lambdaworks_math::field::fields::fft_friendly::stark_252_prime_field::Stark252PrimeField;
+use lambdaworks_math::traits::{Deserializable, Serializable};
 use lambdaworks_stark::cairo::runner::run::{generate_prover_args, CairoVersion};
-use lambdaworks_stark::starks::prover::prove;
+use lambdaworks_stark::starks::proof::StarkProof;
+use lambdaworks_stark::starks::{prover::prove, verifier::verify};
 use rustler::Binary;
 
 const GRINDING_FACTOR: u8 = 10;
@@ -19,6 +21,35 @@ pub fn run_program_and_get_proof_internal(program_content: &[u8]) -> Vec<u8> {
     let proof = prove(&main_trace, &cairo_air, &mut pub_inputs).unwrap();
     let ret: Vec<u8> = proof.serialize();
     ret
+}
+
+pub fn verify_internal<F, A>(
+    proof_bytes: &[u8], /*, air: &A, public_input: &A::PublicInput*/
+) -> bool
+//where
+//    F: IsFFTField,
+//    A: AIR<Field = F>,
+//    FieldElement<F>: ByteConversion,
+{
+    // At this point, the verifier only knows about the serialized proof, the proof options
+    // and the public inputs.
+    let proof = StarkProof::<Stark252PrimeField>::deserialize(&proof_bytes).unwrap();
+
+    /*
+    // The same proof configuration as used in the `generate_prover_args` function.
+    let proof_options = ProofOptions {
+        blowup_factor: 4,
+        fri_number_of_queries: 3,
+        coset_offset: 3,
+        grinding_factor: 1,
+    };
+
+    let air = CairoAIR::new(proof_options, proof.trace_length, pub_inputs.clone(), false);
+
+    assert!(verify(&proof, &air, &pub_inputs));
+    */
+
+    true
 }
 
 #[cfg(test)]
