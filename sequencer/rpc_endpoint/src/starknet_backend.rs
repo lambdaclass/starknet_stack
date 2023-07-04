b63fbf6d4c30dd0824383d5da642839c35669964
Javier Chatruc <jrchatruc@gmail.com>
2023-07-04 17:03:34 -0300

    Mock rpc endpoint api

--- sequencer/rpc_endpoint/src/starknet_backend.rs
+++ sequencer/rpc_endpoint/src/starknet_backend.rs
@@ -1,14 +1,17 @@
+use crate::starknet_backend::MaybePendingBlockWithTxs::Block;
 use cairo_felt::Felt252;
 use jsonrpsee::core::{async_trait, RpcResult};
 
 use crate::rpc::{
-    BlockHashAndNumber, BlockId, BlockWithInternalTransactions, BroadcastedDeclareTransaction,
-    BroadcastedDeployAccountTransaction, BroadcastedInvokeTransaction, BroadcastedTransaction,
-    ContractClass, DeclareTransactionResult, DeployAccountTransactionResult, EventFilterWithPage,
-    EventsPage, FeeEstimate, FunctionCall, InvokeTransactionResult, MaybePendingBlockWithTxHashes,
+    BlockHashAndNumber, BlockId, BlockWithInternalTransactions, BlockWithTxHashes, BlockWithTxs,
+    BroadcastedDeclareTransaction, BroadcastedDeployAccountTransaction,
+    BroadcastedInvokeTransaction, BroadcastedTransaction, ContractClass, DeclareTransactionResult,
+    DeployAccountTransactionResult, EventFilterWithPage, EventsPage, FeeEstimate, FunctionCall,
+    InvokeTransaction, InvokeTransactionResult, InvokeTransactionV1, MaybePendingBlockWithTxHashes,
     MaybePendingBlockWithTxs, MaybePendingTransactionReceipt, StarknetRpcApiServer, StateUpdate,
     SyncStatusType, Transaction,
 };
+use crate::starknet_backend::InvokeTransaction::V1;
 
 pub struct StarknetBackend;
 
@@ -16,7 +19,7 @@ pub struct StarknetBackend;
 #[allow(unused_variables)]
 impl StarknetRpcApiServer for StarknetBackend {
     fn block_number(&self) -> RpcResult<u64> {
-        unimplemented!();
+        Ok(1)
     }
 
     fn block_hash_and_number(&self) -> RpcResult<BlockHashAndNumber> {
@@ -94,8 +97,29 @@ impl StarknetRpcApiServer for StarknetBackend {
     }
 
     /// Get block information with full transactions given the block id
-    fn get_block_with_txs(&self, block_id: BlockId) -> RpcResult<MaybePendingBlockWithTxs> {
-        unimplemented!();
+    fn get_block_with_txs(&self, _block_id: BlockId) -> RpcResult<MaybePendingBlockWithTxs> {
+        let invoke_transaction = InvokeTransactionV1 {
+            transaction_hash: Felt252::new(1920310231),
+            max_fee: Felt252::new(89853483),
+            signature: vec![Felt252::new(183728913)],
+            nonce: Felt252::new(762716321),
+            sender_address: Felt252::new(91232018),
+            calldata: vec![Felt252::new(8126371)],
+        };
+
+        let v1_transaction = V1(invoke_transaction);
+
+        let block = BlockWithTxs {
+            status: crate::rpc::BlockStatus::AcceptedOnL2,
+            block_hash: Felt252::new(11239218),
+            parent_hash: Felt252::new(19203123),
+            block_number: 1,
+            new_root: Felt252::new(938938281),
+            timestamp: 1688498274,
+            sequencer_address: Felt252::new(12039102),
+            transactions: vec![crate::rpc::types::Transaction::Invoke(v1_transaction)],
+        };
+        Ok(Block(block))
     }
 
     /// Returns the chain id.
