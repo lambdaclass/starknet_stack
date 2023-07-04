0e2e6c90bd0439646778ca750ddb9ee0a7d3c6ba
Javier Chatruc <jrchatruc@gmail.com>
2023-07-04 18:16:47 -0300

    Make the watcher dispatcher call the node's rpc api

--- watcher_dispatcher/lib/watcher_dispatcher_poller.ex
+++ watcher_dispatcher/lib/watcher_dispatcher_poller.ex
@@ -1,15 +1,16 @@
 defmodule WatcherDispatcher.Poller do
   use GenServer
   use Tesla
-  alias Phoenix.Endpoint.Watcher
   alias WatcherDispatcher.NIF
-  plug(Tesla.Middleware.BaseUrl, "http://localhost")
+  plug(Tesla.Middleware.BaseUrl, "http://localhost:5000")
   plug(Tesla.Middleware.JSON)
+  alias WatcherDispatcher.Rpc
 
   require Logger
   alias WatcherDispatcher.S3
 
   @polling_frequency_ms 10_000
+  @number_of_blocks_for_confirmation 0
 
   def start_link(args) do
     GenServer.start_link(__MODULE__, args)
@@ -18,26 +19,13 @@ defmodule WatcherDispatcher.Poller do
   @impl true
   def init(_opts) do
     state = %{
-      unconfirmed_blocks: [],
-      highest_block: 0,
-      proof_results: []
+      first_unconfirmed_block_number: 1,
+      highest_block: 0
     }
 
     Process.send_after(self(), :poll, @polling_frequency_ms)
 
     {:ok, state}
-  end
-
-  def fetch_height() do
-    {:ok, %{body: %{"value" => highest_block}}} = post("/starknet", %{"method" => "blockNumber"})
-    {:ok, highest_block}
-  end
-
-  defp fetch_block(height) do
-    {:ok, %{body: %{"value" => [block]}}} =
-      post("/starknet", %{method: "getBlocksFromHeight", params: %{n: 1, height: height}})
-
-    {:ok, block}
   end
 
   @doc """
@@ -47,59 +35,54 @@ defmodule WatcherDispatcher.Poller do
   @impl true
   def handle_info(
         :poll,
-        state = %{unconfirmed_blocks: blocks, highest_block: curr_height, proof_results: results}
+        state = %{
+          first_unconfirmed_block_number: first_unconfirmed_block_number,
+          highest_block: highest_block
+        }
       ) do
-    # {:ok, fetched_height} = fetch_height()
+    {:ok, latest_block_number} = Rpc.last_block_number()
 
-    # # Update known latest height if
-    # # it has increased, and then fetch a block.
-    # state =
-    #   cond do
-    #     curr_height < fetched_height ->
-    #       new_block = fetch_block(fetched_height)
-    #       %{state | highest_block: fetched_height, unconfirmed_blocks: [new_block | blocks]}
+    updated_highest_block =
+      if latest_block_number > highest_block do
+        latest_block_number
+      else
+        highest_block
+      end
 
-    #     true ->
-    #       state
-    #   end
+    if first_unconfirmed_block_number - @number_of_blocks_for_confirmation >= highest_block do
+      {:ok, block} = Rpc.get_block_by_number(first_unconfirmed_block_number)
 
-    # # Group blocks based on their height,
-    # # we'll consider 'confirmed' as 10 blocks into the chain.
-    # %{unconfirmed: unconfirmed_blocks, confirmed: confirmed_blocks} =
-    #   state.unconfirmed_blocks
-    #   |> Enum.group_by(fn %{"block_number" => num} ->
-    #     cond do
-    #       num + 10 < state.curr_height -> :confirmed
-    #       true -> :unconfirmed
-    #     end
-    #   end)
+      Logger.info(
+        "Running proof for block #{block["block_hash"]} with contents #{inspect(block)}"
+      )
 
-    # # For each block, run the proof.
-    # results =
-    #   for block <- confirmed_blocks, into: results do
-    #     run_proofs(block)
-    #   end
+      # TODO: fetch executions from the invoke transactions for this block to prove
+      {:ok, program} = File.read("./programs/fibonacci_cairo1.casm")
 
-    Logger.info("Running proof for block...")
+      proof = run_proofs(program)
 
-    {:ok, program} = File.read("./programs/fibonacci_cairo1.casm")
+      Logger.info("Generated block proof #{inspect(proof)}")
 
-    proof = run_proofs(program)
+      # TODO: Uncomment this when we are ready
+      # :ok = S3.upload_object!(:erlang.list_to_binary(proof), block["block_hash"])
+      Logger.info("Uploaded proof of block with id #{block["block_hash"]}")
 
-    Logger.info("Generated block proof #{inspect(proof)}")
-
-    proof_id = UUID.uuid4()
-    # TODO: Uncomment this when we are ready
-    # :ok = S3.upload_object!(:erlang.list_to_binary(proof), proof_id)
-    Logger.info("Uploaded proof of block with id #{proof_id}")
-
-    Process.send_after(self(), :poll, @polling_frequency_ms)
-
-    {:noreply, %{state | unconfirmed_blocks: [], proof_results: proof}}
+      {:noreply,
+       %{
+         state
+         | highest_block: updated_highest_block,
+           first_unconfirmed_block_number: first_unconfirmed_block_number + 1
+       }}
+    else
+      {:noreply,
+       %{
+         state
+         | highest_block: updated_highest_block,
+           first_unconfirmed_block_number: first_unconfirmed_block_number
+       }}
+    end
   end
 
-  # TODO: Complete this
-  # For now, just call the dummy function
   def run_proofs(block) do
     NIF.run_program_and_get_proof(block)
   end
