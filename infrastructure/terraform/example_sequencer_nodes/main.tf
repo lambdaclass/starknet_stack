de293b9db10d74b6a4fec8dbad87c0bde248cb48
Klaus Lungwitz <18153834+klaus993@users.noreply.github.com>
2023-07-17 17:23:06 -0300

    Remove default for instance_type
    And use graviton3 instance for sequencer

--- infrastructure/terraform/example_sequencer_nodes/main.tf
+++ infrastructure/terraform/example_sequencer_nodes/main.tf
@@ -1,7 +1,8 @@
 module "sequencer_cluster_1" {
   source = "../modules/aws_ec2_cluster"
 
-  cluster_name = "kraken_sequencer_1"
-  ssh_key_name = "klaus"
-  node_count = 4
+  cluster_name  = "starknet_stack_sequencer_0"
+  ssh_key_name  = "klaus"
+  node_count    = 4
+  instance_type = "c7g.xlarge"
 }
--- infrastructure/terraform/modules/aws_ec2_cluster/variables.tf
+++ infrastructure/terraform/modules/aws_ec2_cluster/variables.tf
@@ -6,7 +6,6 @@ variable "cluster_name" {
 variable "instance_type" {
   type = string
   description = "AWS EC2 instance type for the nodes"
-  default = "c5ad.xlarge"
 }
 
 variable "ssh_key_name" {
