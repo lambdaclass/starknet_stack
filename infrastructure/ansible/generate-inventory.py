4e9eb24ea225835b90e0e96d88c1882dc3693cd3
samoht9277 <totobighouse@gmail.com>
2023-07-17 18:03:19 -0300

    Fixed string formatting bug.

--- infrastructure/ansible/generate-inventory.py
+++ infrastructure/ansible/generate-inventory.py
@@ -6,10 +6,10 @@ from sys import argv
 ip_list = argv[1:]
 
 node = {
-    "ansible_host": "{ip}",
+    "ansible_host": "{IP}",
     "ansible_user": "admin", 
     "ansible_python_interpreter": "/usr/bin/python3",
-    "ansible_ssh_private_key_file": "{{ lookup(\"env\",\"ANSIBLE_SSH_PKEY\") }}",
+    "ansible_ssh_private_key_file": "{ANSIBLE_SSH_PKEY}",
     "ansible_ssh_extra_args": "-o StrictHostKeyChecking=no"
 }
 
@@ -18,7 +18,7 @@ obj = {"nodes": {"hosts": {}}}
 for i in range(0, len(ip_list)):
     obj["nodes"]["hosts"]["node_%s" % i] = dict()
     for key in node:
-        obj["nodes"]["hosts"]["node_%s" % i][key] = node[key].format(ip = ip_list[i])
+        obj["nodes"]["hosts"]["node_%s" % i][key] = node[key].format(IP = ip_list[i], ANSIBLE_SSH_PKEY = "{{ lookup('env','ANSIBLE_SSH_PKEY') }}")
 
 with open("inventory.yaml", "w") as file:
     file.write(dump(obj))
