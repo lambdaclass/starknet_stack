335167369b734f4f4f18c6030339bcc3b46f930b
Klaus Lungwitz <18153834+klaus993@users.noreply.github.com>
2023-07-19 19:14:43 -0300

    Remove migrate

--- watcher_prover/entrypoint.sh
+++ watcher_prover/entrypoint.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 set -ex
 
-/home/admin/starknet_stack/watcher_prover/_build/prod/rel/watcher_prover/bin/watcher_prover migrate
 /home/admin/starknet_stack/watcher_prover/_build/prod/rel/watcher_prover/bin/watcher_prover start
