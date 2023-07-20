56930cff4dc739c9b151ba8cbeabe09d0a337f58
Klaus Lungwitz <18153834+klaus993@users.noreply.github.com>
2023-07-20 12:01:22 -0300

    Add some error handling for the IP string list argument
    and fix typo on makefile

--- sequencer/config/scripts/generate_committee.py
+++ sequencer/config/scripts/generate_committee.py
@@ -1,25 +1,35 @@
 #!/usr/bin/env python3
 
-import subprocess, sys
+import subprocess, sys, ipaddress
 
 from config import RemoteCommittee, Key
+
+def parse_ip_string(ip_string):
+    ip_list = [ip.strip() for ip in ip_string.split(',')]
+    for ip in ip_list:
+        try:
+            ipaddress.ip_address(ip)
+        except ValueError:
+            print(f'{ip} is not a valid IP address')
+            sys.exit(1)
+    return ip_list
 
 def generate_config(ips):
     ''' '''
     # Generate configuration files.
     keys = []
-    ips = [ip.strip() for ip in ips.split(',')]
+    ip_list = parse_ip_string(ips)
     base_port = 9000
-    key_files = [f'sequencer_node{i}.json' for i in range(len(ips))]
+    key_files = [f'sequencer_node{i}.json' for i in range(len(ip_list))]
     for filename in key_files:
         keys += [Key.from_file(f'../{filename}')]
 
     names = [x.name for x in keys]
-    committee = RemoteCommittee(names, base_port, ips)
-    committee.print('../committee.json')
+    committee = RemoteCommittee(names, base_port, ip_list)
+    committee.print('./committee.json')
 
 if len(sys.argv) != 2:
-  print("You need to pass 1 argument, a quoted string with the list of comma-separated ips")
-  sys.exit(1)
+    print("You need to pass 1 argument: a quoted string with a list of comma-separated IPs")
+    sys.exit(1)
 
 generate_config(sys.argv[1])
