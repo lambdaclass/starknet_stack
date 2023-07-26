031e512325e23d0a746065f8a8f5a99b5e2dfb9e
Klaus Lungwitz <18153834+klaus993@users.noreply.github.com>
2023-07-26 13:33:26 -0300

    Parametrize terraform backend values

--- infrastructure/terraform/example_sequencer_nodes/providers.tf
+++ infrastructure/terraform/example_sequencer_nodes/providers.tf
@@ -1,9 +1,12 @@
 terraform {
   backend "s3" {
-    bucket = "terraform-lambdaclass"
-    key    = "kraken_sequencer/terraform.tfstate"
+    bucket = var.terraform_aws_s3_bucket
+    key    = "${ var.terraform_aws_s3_directory }/terraform.tfstate"
     region = "us-west-2"
   }
 
   required_version = ">= 1.2.0"
 }
+
+variable "terraform_aws_s3_bucket" {}
+variable "terraform_aws_s3_directory" {}
