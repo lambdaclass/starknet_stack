8b058800a3e4a5cf2d8bc576170d6bbc32a6741f
Klaus Lungwitz <18153834+klaus993@users.noreply.github.com>
2023-07-26 13:15:39 -0300

    WIP: automate N node deployment

--- infrastructure/terraform/example_sequencer_nodes/main.tf
+++ infrastructure/terraform/example_sequencer_nodes/main.tf
@@ -6,3 +6,22 @@ module "sequencer_cluster_1" {
   node_count    = 4
   instance_type = "c6a.large"
 }
+
+module "sequencer_cluster_2" {
+  source = "../modules/aws_ec2_cluster"
+
+  cluster_name  = var.cluster_name
+  ssh_key_name  = var.ssh_key_name
+  node_count    = var.node_count
+  instance_type = var.instance_type
+
+  aws_ami_id = var.aws_ami_id
+}
+
+variable "cluster_name" {}
+variable "ssh_key_name" {}
+variable "node_count" {}
+variable "instance_type" {}
+variable "aws_ami_id" {
+  default = "ami-0acfc42b227d0b719"
+}
