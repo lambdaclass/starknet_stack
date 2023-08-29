724ece2f7420bc2949e4ed52a597a11042f382e4
Nacho <amigoigna@gmail.com>
2023-08-29 13:06:33 -0300

    remove older calls

--- sequencer/execution_engine/src/starknet_in_rust_engine.rs
+++ sequencer/execution_engine/src/starknet_in_rust_engine.rs
@@ -10,9 +10,6 @@ use std::{collections::HashMap, sync::Arc};
 
 pub struct StarknetState {
     state: CachedState<InMemoryStateReader>,
-    fib_entrypoint_selector: Felt252,
-    fact_entrypoint_selector: Felt252,
-    erc20_entrypoint_selector: Felt252,
 }
 
 impl StarknetState {
@@ -20,7 +17,7 @@ impl StarknetState {
         let mut contract_class_cache = HashMap::new();
         let mut state_reader = InMemoryStateReader::default();
 
-        let fib_entrypoint_selector = {
+        let _fib_entrypoint_selector = {
             let contract_address = Address(0.into());
             let class_hash: ClassHash = [1; 32];
 
@@ -46,7 +43,7 @@ impl StarknetState {
                 .clone()
         };
         // pre-add factorial
-        let fact_entrypoint_selector = {
+        let _fact_entrypoint_selector = {
             let program_data: &[u8] = include_bytes!("../../cairo_programs/fact_contract.casm");
             let contract_class: CasmContractClass = serde_json::from_slice(program_data).unwrap();
 
@@ -66,7 +63,7 @@ impl StarknetState {
             entrypoints.external.get(0).unwrap().selector.clone()
         };
 
-        let erc20_entrypoint_selector = {
+        let _erc20_entrypoint_selector = {
             // data to deploy
             let contract_address = Address(2.into());
             let erc20_class_hash: ClassHash = [3; 32];
@@ -101,9 +98,6 @@ impl StarknetState {
 
         StarknetState {
             state,
-            fib_entrypoint_selector: fib_entrypoint_selector.into(),
-            fact_entrypoint_selector: fact_entrypoint_selector.into(),
-            erc20_entrypoint_selector: erc20_entrypoint_selector.into(),
         }
     }
 
@@ -139,119 +133,12 @@ impl StarknetState {
 
         invoke_tx_execution
     }
-
-
-    // deprecated/can be deleted
-    pub fn execute_fibonacci(&mut self, n: usize) -> Vec<Felt252> {
-        let invoke_tx = InvokeFunction::new(
-            Address(0.into()),
-            self.fib_entrypoint_selector.clone(),
-            u128::MAX,
-            Felt252::new(0),
-            vec![1.into(), 1.into(), n.into()],
-            vec![],
-            Felt252::new(0),
-            None,
-        )
-        .unwrap();
-
-        let return_data = invoke_tx
-            .create_for_simulation(false, false, true, true)
-            .execute(&mut self.state, &BlockContext::default(), u128::MAX)
-            .unwrap();
-        return_data.call_info.unwrap().retdata
-    }
-
-    // deprecated/can be deleted
-    pub fn execute_factorial(&mut self, n: usize) -> Vec<Felt252> {
-        let invoke_tx = InvokeFunction::new(
-            Address(1.into()),
-            self.fact_entrypoint_selector.clone(),
-            u128::MAX,
-            Felt252::new(0),
-            vec![n.into()],
-            vec![],
-            Felt252::new(0),
-            None,
-        )
-        .unwrap();
-
-        let return_data = invoke_tx
-            .create_for_simulation(false, false, true, true)
-            .execute(&mut self.state, &BlockContext::default(), u128::MAX)
-            .unwrap();
-        return_data.call_info.unwrap().retdata
-    }
-
-    // deprecated/can be deleted
-    pub fn execute_erc20(
-        &mut self,
-        initial_supply: Felt252,
-        symbol: Felt252,
-        contract_address: Felt252,
-    ) {
-        let name_ = Felt252::from_bytes_be(b"some-token");
-        let decimals_ = Felt252::from(24);
-        let erc20_salt = felt_str!("1234");
-
-        let calldata = vec![
-            erc20_salt,
-            contract_address,
-            name_,
-            decimals_,
-            initial_supply,
-            symbol,
-        ];
-
-        let invoke_tx = InvokeFunction::new(
-            Address(2.into()),
-            self.erc20_entrypoint_selector.clone(),
-            u128::MAX,
-            Felt252::new(0),
-            calldata,
-            vec![],
-            Felt252::new(0),
-            None,
-        )
-        .unwrap();
-
-        let return_data = invoke_tx
-            .create_for_simulation(false, false, true, true)
-            .execute(&mut self.state, &BlockContext::default(), u128::MAX)
-            .unwrap();
-        // arguments of deploy contract
-        println!("{:?}", return_data);
-    }
 }
 
 #[cfg(test)]
 mod tests {
     use cairo_felt::{felt_str, Felt252};
-
     use crate::starknet_in_rust_engine::StarknetState;
-
-    #[test]
-    fn execute_fibonacci_of_10() {
-        let mut starknet_state = StarknetState::new_for_tests();
-
-        let fib_of_10 = starknet_state.execute_fibonacci(10);
-        assert_eq!(fib_of_10, vec![89.into()]);
-    }
-
-    #[test]
-    fn execute_erc20() {
-        let mut starknet_state = StarknetState::new_for_tests();
-
-        starknet_state.execute_erc20(2.into(), Felt252::from_bytes_be(b"smbl"), 10000.into());
-    }
-
-    #[test]
-    fn execute_fact_of_10() {
-        let mut starknet_state = StarknetState::new_for_tests();
-
-        let return_data = starknet_state.execute_factorial(10);
-        assert_eq!(return_data, vec![3628800.into()]);
-    }
 
     #[test]
     fn test_three_contracts() {
@@ -305,6 +192,7 @@ mod tests {
             "213cda0181d4bd6d07f2e467ddf45a1d971e14ca1bcd4c83949a6d830a15b7f",
             16
         );
+        
         starknet_state
             .invoke(vec![Felt252::new(9999), selector, Felt252::new(2000)])
             .unwrap_err();
